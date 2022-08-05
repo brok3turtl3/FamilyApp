b65b5c83f45faf80e675dd0b0fe5c703ab2635c7
Brok3Turtl3 <brok3turtl3@gmail.com>
2022-08-05 11:04:07 -0400

    Create Post model

--- models/Profile.js
+++ models/Profile.js
@@ -1,54 +1,54 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const ProfileSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user'
-  },
-  city: {
-    type: String
-  },
-  bio: {
-    type: String
-  },
-  work: {
-    company: {
-      type: String
-    },
-    position: {
-      type: String
-    }
-
-  },
-  education : {
-    school: {
-      type: String
-    },
-    program: {
-      type: String
-    }
-  },
-  interests: {
-    type: [String]
-  },
-  dob : {
-    type: Date
-  },
-  social: {
-    facebook: {
-      type: String
-    },
-    instagram: {
-      type: String
-    },
-    twitter: {
-      type: String
-    }
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+const ProfileSchema = new Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'users',
+	},
+	city: {
+		type: String,
+	},
+	bio: {
+		type: String,
+	},
+	work: {
+		company: {
+			type: String,
+		},
+		position: {
+			type: String,
+		},
+	},
+	education: {
+		school: {
+			type: String,
+		},
+		program: {
+			type: String,
+		},
+	},
+	interests: {
+		type: [String],
+	},
+	dob: {
+		type: Date,
+	},
+	social: {
+		facebook: {
+			type: String,
+		},
+		instagram: {
+			type: String,
+		},
+		twitter: {
+			type: String,
+		},
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
