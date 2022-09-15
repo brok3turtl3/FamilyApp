6239e5c080e4daaaad0a5b09a23d0194e36fbdad
Brok3Turtl3 <brok3turtl3@gmail.com>
2022-09-15 11:53:25 -0400

    Add profile image to SmallProfile component

--- client/src/components/profiles/SmallProfile.js
+++ client/src/components/profiles/SmallProfile.js
@@ -5,18 +5,26 @@ import { Link } from 'react-router-dom';
 const SmallProfile = ({
 	profile: {
 		user: { _id, name },
+		image,
 		city,
 		bio,
 	},
 }) => {
 	return (
 		<div className='small-profile'>
+{image !== null ? (
+							<div className="profile-img">
+							<img src={`../../${image}`} alt="Placeholder"></img>
+							</div>) : null}
+			<div className="sm-pr-info">
 			<div className="sm-pr-field"><h2>{name}</h2></div>
 			<div className="sm-pr-field"><h1>{city}</h1></div>
 			<div className="sm-pr-field"><p>{bio}</p></div>
 			<Link to={`/profile/${_id}`} className='btn btn-primary'>
 				Detailed Profile
 			</Link>
+			
+		</div>
 		</div>
 	);
 };
